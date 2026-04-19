f8c72a3a9c515bfbf05c56235c1473636fe025ae
spattersiva <ms7018394@gmail.com>
2026-04-19 19:14:25 +0530

    updated model for cpu friendly

--- model.py
+++ model.py
@@ -1,16 +1,21 @@
 import torch
 from diffusers import StableDiffusionPipeline
+import streamlit as st
 
 # Load model once (important for Streamlit)
-pipe = StableDiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5",
-    torch_dtype=torch.float16
-).to("cpu")
+@st.cache_resource
+def load_model():
+    return StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipe=load_model()
 
+pipe.enable_attention_slicing()
+pipe.enable_sequential_cpu_offload()
+
+pipe = load_model()
 def generate_image(prompt):
     image = pipe(
         prompt,
-        num_inference_steps=25
+        num_inference_steps=15
     ).images[0]
 
     image_path = "generated.png"
